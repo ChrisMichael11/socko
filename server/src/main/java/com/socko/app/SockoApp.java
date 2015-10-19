c222eab0b989fb3abaa46ef000c27adf439e67e8
Chris Michael <christopher.m.michael@gmail.com>
2015-10-18 20:18:12 -0700

    Updating notes file, adding socko "classes" file

--- server/src/main/java/com/socko/app/SockoApp.java
+++ server/src/main/java/com/socko/app/SockoApp.java
@@ -5,12 +5,13 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
 
 public class SockoApp extends Application<SockoAppConfiguration> {
-
+//  SockoApp has characteristics of the application SockoAppConfiguration along with info below
     public static void main(String[] args) throws Exception {
         new SockoApp().run(args);
     }
 
     @Override
+    //Instructs the compiler that you intend to override a method (SockoAppConfiguration) in the superclass
     public void run(SockoAppConfiguration configuration, Environment environment) throws Exception {
         System.out.println("Socko app running");
     }
